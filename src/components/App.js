179e8ec6d2c8eb7b6bbcec556c44140ef9a68e9b
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-10-21 21:03:33 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: false,
-      info: ""
+      display: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
   handlClick() {
     this.setState({
-      display: true,
-      info:
-        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+      display: true
     });
   }
   render() {
     return (
       <div id="main">
         <button id="click" onClick={this.handlClick}></button>
-        {this.state.display && <p id="para">{this.state.info}</p>}
+        {this.state.display && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
       </div>
     );
   }
