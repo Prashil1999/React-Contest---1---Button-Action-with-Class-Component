6c3183594decfe0c2663491de5882ea4e063ab42
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-10-23 12:44:28 +0530

    ch

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,53 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isDisplay: false
+//     };
+//     this.info =
+//       "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+//   }
+//   handlClick() {
+//     this.setState({
+//       isDisplay: true
+//     });
+//   }
+//   render() {
+//     return (
+//       <div id="main">
+//         <button id="click" onClick={this.handlClick}></button>
+//         {this.state.isDisplay === true ? <p id="para">{this.info}</p> : null}
+//       </div>
+//     );
+//   }
+// }
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isDisplay: false
+      showPara: false
     };
-    this.info =
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    this.handleClick = this.handleClick.bind(this);
   }
-  handlClick() {
+  handleClick() {
     this.setState({
-      isDisplay: true
+      showPara: true
     });
   }
   render() {
     return (
       <div id="main">
-        <button id="click" onClick={this.handlClick}></button>
-        {this.state.isDisplay === true ? <p id="para">{this.info}</p> : null}
+        <button id="click" onClick={this.handleClick}></button>
+        {this.state.showPara && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
       </div>
     );
   }
