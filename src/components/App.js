a2784aeccca1d5727ece0443ddc7107de899c4ef
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-10-23 12:30:17 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,21 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isDisplay: false,
-      info: ""
+      isDisplay: false
     };
+    this.info =
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
   }
   handlClick() {
     this.setState({
-      isDisplay: true,
-      info:
-        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+      isDisplay: true
     });
   }
   render() {
     return (
       <div id="main">
         <button id="click" onClick={this.handlClick}></button>
-        {this.state.display && <p id="para">{this.state.info}</p>}
+        {this.state.isDisplay === true ? <p id="para">{this.info}</p> : null}
       </div>
     );
   }
